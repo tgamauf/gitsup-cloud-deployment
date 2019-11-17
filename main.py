100d1b6329a09fdba7e54897cd68f3afdb44abfd
Thomas Gamauf <thomas.gamauf@gkcs.eu>
2019-11-17 17:54:49 +0100

    Implement required encryption of secrets

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import base64
 from flask import abort
 import hmac
 from http import HTTPStatus
 from gitsup import update_git_submodules
+import googleapiclient.discovery
 import os
 import sys
 
@@ -13,7 +15,9 @@ def autoupdate(request):
     See https://developer.github.com/webhooks/creating/.
     """
 
-    _check_signature(request)
+    github_api_token, github_webhook_secret = _read_secrets()
+
+    _check_signature(github_webhook_secret, request)
 
     request_json = request.get_json()
     try:
@@ -24,7 +28,7 @@ def autoupdate(request):
               file=sys.stderr)
 
     try:
-        update_git_submodules()
+        update_git_submodules(token=github_api_token)
     except ConnectionError as e:
         print(f"{e}", file=sys.stderr)
         abort(HTTPStatus.SERVICE_UNAVAILABLE)
@@ -32,14 +36,48 @@ def autoupdate(request):
         print(f"{e}", file=sys.stderr)
         abort(HTTPStatus.INTERNAL_SERVER_ERROR)
 
-    # Update succeeded
     return "ok"
 
 
-def _check_signature(request):
-    """ Check if we received the correct signature. """
+def _read_secrets():
+    """
+    Get the secrets from the environment. They are stored encrypted, so
+    we need to use the Google Key Management Service (KMS) to decrypt
+    them.
+    """
 
-    secret = os.environ["SECRET_TOKEN"]
+    crypto_key_id = os.environ["KMS_CRYPTO_KEY_ID"]
+    encrypted_github_api_token = os.environ["GITHUB_API_TOKEN"]
+    encrypted_github_webhook_secret = os.environ["GITHUB_WEBHOOK_SECRET"]
+
+    kms_client = googleapiclient.discovery.build("cloudkms", "v1")
+
+    github_api_token = _decrypt(kms_client,
+                                crypto_key_id,
+                                encrypted_github_api_token)
+    github_webhook_secret = _decrypt(kms_client,
+                                     crypto_key_id,
+                                     encrypted_github_webhook_secret)
+
+    return github_api_token, github_webhook_secret
+
+
+def _decrypt(client, crypto_key_id, encrypted_secret):
+    """ Decrypt a secret using the Google Key Management Service. """
+
+    response = client \
+        .projects() \
+        .locations() \
+        .keyRings() \
+        .cryptoKeys() \
+        .decrypt(name=crypto_key_id, body={"ciphertext": encrypted_secret}) \
+        .execute()
+
+    return base64.b64decode(response["plaintext"]).decode("utf-8").strip()
+
+
+def _check_signature(secret, request):
+    """ Check if we received the correct signature. """
 
     header_signature = request.headers.get("X-Hub-Signature")
     if not header_signature:
